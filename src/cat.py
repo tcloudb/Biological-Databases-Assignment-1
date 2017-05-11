4c3e68e7cf6578e4040af0d84e9f2c9d4d10bec4
tcloudb <tcloudb@gmail.com>
2017-05-11 11:02:43 -0700

    ??

--- src/cat.py
+++ src/cat.py
@@ -4,13 +4,13 @@ seqLength = 0;
 for line in f:
     if line[0] == ">":
         if seqLength != 0:
-            print seqLength
+            print (seqLength)
             #print
-        print line,
-        seqLength = 0;
+        print (line)
+        seqLength = 0
     else:
         for char in line:
             if char.isalpha():  #is a letter
                 seqLength += 1
-print seqLength
+print (seqLength)
     
